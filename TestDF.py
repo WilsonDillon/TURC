c2c74d222acdb6fa02700654e9afd3781695ad69
WilsonDillon <dillon@gitekunstdesign.com>
2022-07-30 19:25:11 -0500

    Added file structure

--- TestDF.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# resultsDF = pd.DataFrame(columns = ['Actual', 'Predicted'])
-# print(resultsDF)
-new_row = pd.DataFrame([[3, 2.5]], columns=['Actual', 'Predicted'])
-print(new_row)
-# resultsDF = pd.concat([resultsDF, new_row])
-
-# print(resultsDF)
